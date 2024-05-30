ff2fb85a6219fe83bf1d8199166c01c32f911d55
AtsideR <redistaizza@gmail.com>
2024-05-30 21:36:34 +0700

    tugas akhir ACC
    Signed-off-by: AtsideR <redistaizza@gmail.com>

--- ta.py
+++ ta.py
@@ -4,7 +4,6 @@ from tkinter.ttk import Combobox
 import pandas as pd
 
 class jadwalkuliah:
-    #
     def __init__(self, master):
         self.master = master
         self.master.title("Pembuat Jadwal Kuliah")
@@ -17,7 +16,6 @@ class jadwalkuliah:
         self.set_theme()
         self.buat_widgets()
 
-    # function untuk mengatur tema
     def set_theme(self):
         if self.mode_gelap:
             self.bg_color = "#2c3e50"
@@ -32,27 +30,22 @@ class jadwalkuliah:
 
         self.master.configure(bg=self.bg_color)
 
-    # untuk membuat widget2/tool2 pada program
     def buat_widgets(self):
         self.text_area_hari = {}
         self.selected_day = tk.StringVar()
-        
-        # grid = untuk mengatur lokasi tool pada program
-        # label = untuk menuliskan kata yang akan muncul di program
-        # combobox = untuk membuat tombol list pada program
-        # button = untuk membuat tombol di program
+
         for i, hari in enumerate(self.hari):
             label1 = tk.Label(self.master, text=f"Jadwal {hari}", bg=self.bg_color, fg=self.fg_color, font=("Arial", 12, "bold"))
             label1.grid(row=1, column=i, padx=5, pady=5, sticky="nsew")
             text_area = tk.Text(self.master, height=25, width=20, state="disabled", bg=self.text_area_color, fg=self.fg_color, font=("Arial", 10))
             text_area.grid(row=2, column=i, padx=5, pady=5, sticky="nsew")
             self.text_area_hari[hari] = text_area
-            
+
         label2 = tk.Label(self.master, text="PEMBUAT JADWAL", bg=self.bg_color, fg=self.fg_color, font=("Arial", 12, "bold"))
         label2.grid(row=0, column=len(self.hari)//2, padx=5, pady=5, sticky="nsew")
         
         tk.Label(self.master, text="Pilih Hari:", bg=self.bg_color, fg=self.fg_color, font=("Arial", 10)).grid(row=5, column=0, padx=5, pady=5, sticky="e")
-        self.menu_hari = Combobox(self.master, height= 5, width= 8, textvariable=self.selected_day, values=self.hari, font=("Arial", 10), state= "readonly")
+        self.menu_hari = Combobox(self.master, height=5, width=8, textvariable=self.selected_day, values=self.hari, font=("Arial", 10), state="readonly")
         self.menu_hari.grid(row=5, column=1, padx=5, pady=5, sticky="w")
 
         tk.Label(self.master, text="Masukkan jadwal:", bg=self.bg_color, fg=self.fg_color, font=("Arial", 10)).grid(row=6, column=0, padx=5, pady=5, sticky="e")
@@ -62,40 +55,39 @@ class jadwalkuliah:
         tk.Label(self.master, text="Jam Mulai:", bg=self.bg_color, fg=self.fg_color, font=("Arial", 10)).grid(row=7, column=0, padx=5, pady=5, sticky="e")
         self.jam_mulai_var = tk.StringVar()
         self.menit_mulai_var = tk.StringVar()
-        self.menu_jam_mulai = Combobox(self.master, height= 5, width= 3, textvariable=self.jam_mulai_var, values=self.jam, font=("Arial", 10), state= "readonly")
+        self.menu_jam_mulai = Combobox(self.master, height=5, width=3, textvariable=self.jam_mulai_var, values=self.jam, font=("Arial", 10), state="readonly")
         self.menu_jam_mulai.grid(row=7, column=1, padx=5, pady=5, sticky="w")
-        self.menu_menit_mulai = Combobox(self.master, height= 5, width= 3, textvariable=self.menit_mulai_var, values=self.menit, font=("Arial", 10), state= "readonly")
+        self.menu_menit_mulai = Combobox(self.master, height=5, width=3, textvariable=self.menit_mulai_var, values=self.menit, font=("Arial", 10), state="readonly")
         self.menu_menit_mulai.grid(row=7, column=2, padx=5, pady=5, sticky="w")
 
         tk.Label(self.master, text="Jam Selesai:", bg=self.bg_color, fg=self.fg_color, font=("Arial", 10)).grid(row=8, column=0, padx=5, pady=5, sticky="e")
         self.jam_selesai_var = tk.StringVar()
         self.menit_selesai_var = tk.StringVar()
-        self.menu_jam_selesai = Combobox(self.master, height= 5, width= 3, textvariable=self.jam_selesai_var, values=self.jam, font=("Arial", 10), state= "readonly")
+        self.menu_jam_selesai = Combobox(self.master, height=5, width=3, textvariable=self.jam_selesai_var, values=self.jam, font=("Arial", 10), state="readonly")
         self.menu_jam_selesai.grid(row=8, column=1, padx=5, pady=5, sticky="w")
-        self.menu_menit_selesai = Combobox(self.master, height= 5, width= 3, textvariable=self.menit_selesai_var, values=self.menit, font=("Arial", 10), state= "readonly")
+        self.menu_menit_selesai = Combobox(self.master, height=5, width=3, textvariable=self.menit_selesai_var, values=self.menit, font=("Arial", 10), state="readonly")
         self.menu_menit_selesai.grid(row=8, column=2, padx=5, pady=5, sticky="w")
         
-
         self.tambah_button = tk.Button(self.master, text="Tambah Acara", command=self.tambah_acara, bg=self.btn_color, fg=self.fg_color, font=("Arial", 10))
         self.tambah_button.grid(row=9, column=len(self.hari)//2, padx=5, pady=5, sticky="nsew")
-        
+
         self.hapus_button = tk.Button(self.master, text="Hapus Jadwal", command=self.pilih_hari_hapus, bg=self.btn_color, fg=self.fg_color, font=("Arial", 10))
         self.hapus_button.grid(row=10, column=len(self.hari)//2, padx=5, pady=5, sticky="nsew")
 
         self.mode_button = tk.Button(self.master, text="Mode Gelap", command=self.toggle_mode, bg=self.btn_color, fg=self.fg_color, font=("Arial", 10))
         self.mode_button.grid(row=11, column=6, padx=5, pady=5, sticky="nsew")
-        # Perbarui teks tombol mode
         self.mode_button.config(text="Mode Terang" if self.mode_gelap else "Mode Gelap")
-        
+
         self.simpan_button = tk.Button(self.master, text="Simpan Jadwal", command=self.simpan_jadwal, bg=self.btn_color, fg=self.fg_color, font=("Arial", 10))
         self.simpan_button.grid(row=11, column=len(self.hari)//2, padx=5, pady=5, sticky="nsew")
 
-        # untuk menkonfigurasi ukuran grid kolom
+        self.total_jadwal_label = tk.Label(self.master, text="Total Jadwal: 0", bg=self.bg_color, fg=self.fg_color, font=("Arial", 10, "bold"))
+        self.total_jadwal_label.grid(row=5, column=6, padx=5, pady=5, sticky="nsew")
+
         for i in range(len(self.hari)):
             self.master.columnconfigure(i, weight=1)
         self.master.rowconfigure((0, 1), weight=1)
 
-    # function buat nambahin acara di textarea
     def tambah_acara(self):
         hari = self.selected_day.get()
         acara = self.acara_entry.get()
@@ -106,12 +98,10 @@ class jadwalkuliah:
             waktu_mulai_menit = int(self.jam_mulai_var.get()) * 60 + int(self.menit_mulai_var.get())
             waktu_selesai_menit = int(self.jam_selesai_var.get()) * 60 + int(self.menit_selesai_var.get())
 
-            # Memeriksa apakah waktu mulai lebih awal dari waktu selesai
             if waktu_mulai_menit >= waktu_selesai_menit:
                 messagebox.showerror("Error", "Waktu mulai harus lebih awal dari waktu selesai.")
                 return
 
-            # Memeriksa apakah ada tabrakan jadwal
             for existing_mulai, existing_selesai, _ in self.jadwal[hari]:
                 existing_mulai_menit = int(existing_mulai.split(':')[0]) * 60 + int(existing_mulai.split(':')[1])
                 existing_selesai_menit = int(existing_selesai.split(':')[0]) * 60 + int(existing_selesai.split(':')[1])
@@ -121,13 +111,13 @@ class jadwalkuliah:
                     return
 
             self.jadwal[hari].append((waktu_mulai, waktu_selesai, acara))
-            self.jadwal[hari].sort(key=lambda x: x[0])  # Urutkan acara berdasarkan waktu mulai
+            self.jadwal[hari].sort(key=lambda x: x[0])
             self.perbarui_jadwal()
             self.acara_entry.delete(0, tk.END)
+            self.update_total_jadwal()
         else:
             messagebox.showerror("Error", "Silakan isi semua kolom.")
 
-    # function untuk memasukan jadwal di textarea
     def perbarui_jadwal(self):
         for hari, text_area in self.text_area_hari.items():
             text_area.config(state="normal")
@@ -136,13 +126,15 @@ class jadwalkuliah:
                 text_area.insert(tk.END, f"{waktu_mulai} - {waktu_selesai}: {acara}\n")
             text_area.config(state="disabled")
 
-    # metode untuk mengubah mode gelap dan terang
+    def update_total_jadwal(self):
+        total_jadwal = sum(len(acara_list) for acara_list in self.jadwal.values())
+        self.total_jadwal_label.config(text=f"Total Jadwal: {total_jadwal}")
+
     def toggle_mode(self):
         self.mode_gelap = not self.mode_gelap
         self.set_theme()
         self.buat_widgets()
 
-    # method untuk membuat penghapus jadwal  
     def pilih_hari_hapus(self):
         self.hapus_window = tk.Toplevel(self.master)
         self.hapus_window.title("Pilih Hari")
@@ -194,6 +186,7 @@ class jadwalkuliah:
                     self.jadwal[hari].remove(jadwal)
                     self.perbarui_jadwal()
                     self.acara_hapus_window.destroy()
+                    self.update_total_jadwal()
                     messagebox.showinfo("Info", "Jadwal berhasil dihapus.")
                     return
             messagebox.showerror("Error", "Jadwal tidak ditemukan.")
